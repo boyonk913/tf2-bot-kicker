3ff4bbcc55cd20602e05c01c3553ea1936c8e41d
boyonk <42876197+boyonkgit@users.noreply.github.com>
2021-04-29 14:43:12 +0200

    v2.0
    - Added auto update
    - Added Regex support
    - Complete rewrite for own clarity

--- code.py
+++ code.py
@@ -1,25 +1,29 @@
-import keyboard
 import os
+import keyboard
+import re
 import requests
 from time import sleep
 
-## Player List
+### variables ------ ###
 players = list()
+commands = list()
+raw_console = list()
 
-## Config Variables
 chars = " !\"#$&'()*-+,./0123456789:;<=>?@[\\]^_{}|~abcdefghijklmnopqrstuvwxyzбвгджзклмнпрстфхцчшщаеёиоуыэюяйьъ"
 path = "C:/Program Files (x86)/Steam/steamapps/common/Team Fortress 2"
 botnames = list()
+botregexnames = list()
 botsteamids = list()
 output_player = False
 output_votekick = True
 clear_log = True
 online_database = True
 sleep_time = 5
-status_keybind = 71
-votekick_keybind = 72
-tf2_status_keybind = "KP_HOME"
-tf2_votekick_keybind = "KP_UPARROW"
+query_keybind = 71
+execute_keybind = 72
+tf2_query_keybind = "KP_HOME"
+tf2_execute_keybind = "KP_UPARROW"
+auto_update = True
 
 key_convert = {
     "LWIN":91,
@@ -48,9 +52,10 @@ key_convert = {
     "KP_INS":82,
     "KP_DEL":83
 }
+### ---------------- ###
 
-# This is used to convert a string with the format hh:mm:ss (or mm:ss) to an integer
-def timetoint(time):
+### util ----------- ###
+def time_to_int(time):
     try:
         total = 0
         times = time.split(":")
@@ -62,29 +67,10 @@ def timetoint(time):
     except:
         return 0
 
-def votekick(player):
-    try:
-        #If we enabled the outputting of votekicks, the console will print a message
-        if output_votekick:
-            print("Votekicking " + str(player))
-        # We open our votekick file, and write the desired command to kick the bot inside
-        with open(path + "/tf/cfg/votekick.cfg", "w", encoding="utf-8", errors="ignore") as f:
-            f.write("callvote kick " + str(player.userid))
-            f.close()
-        # We wait a bit so everything can catch up to speed
-        sleep(1)
-        # We send a key press to the system, which TF2 will read.
-        keyboard.send(votekick_keybind)
-        # TF2 will respond by executing the cfg file, and votekicking the bot
-        sleep(5)
-    except:
-        # Sometimes if we update the file too fast, we can get a PermissionError
-        # If this is the case, we simply wait for a while and try again later
-        print("PermissionError, waiting")
-        sleep(10)
 
 # This is used to find the player with the shortest connectiom time with the name of variable 'name'
-def getnewest(name):
+def get_newest(name):
+    global players
     duplicates = list()
     # We query through every player
     for player in players:
@@ -100,7 +86,7 @@ def getnewest(name):
             newestplayer = duplicate
     return newestplayer
 
-# This is a simple class instance which holds some basic player info
+### classes -------- ###
 class PlayerInstance:
     
     userid = 0
@@ -117,13 +103,23 @@ class PlayerInstance:
     def __str__(self):
         return "PlayerInstance[userid:" + str(self.userid) + ",name:" + self.name + ",time:" + str(self.time) + ",steamid:" + self.steamid + "]"
 
+### ---------------- ###
+
+### setup ---------- ###
 def setup():
-    global tf2_status_keybind
-    global tf2_votekick_keybind
+    read_config()
+    check_update()
+    setup_autoexec()
+    create_bk_autoexec()
+    create_bk_query()
+    get_bots()
+
+
+def read_config():
     with open("config.properties", "r",encoding="utf-8") as f:
         config = f.readlines()
     for line in config:
-        if not line.startswith("#") and not line == "":
+        if not line.startswith("#") and not line.strip() == "":
             splitter = line.find("=")
             key = line[:splitter]
             value = line[splitter+1:].replace("\n","")
@@ -145,60 +141,105 @@ def setup():
             elif key == "sleep_time":
                 global sleep_time
                 sleep_time = int(value)
-            elif key == "status_keybind":
-                global status_keybind
-                status_keybind = key_convert.get(value, value)
-                tf2_status_keybind = value
-            elif key == "votekick_keybind":
-                global votekick_keybind
-                votekick_keybind = key_convert.get(value, value)
-                tf2_votekick_keybind = value
+            elif key == "query_keybind" or key == "status_keybind":
+                global query_keybind
+                global tf2_query_keybind
+                query_keybind = key_convert.get(value, value)
+                tf2_query_keybind = value
+            elif key == "execute_keybind" or key == "votekick_keybind":
+                global execute_keybind
+                global tf2_execute_keybind
+                execute_keybind = key_convert.get(value, value)
+                tf2_execute_keybind = value
             elif key == "online_database":
                 global online_database
                 online_database = value.lower().startswith("t")
+            elif key == "auto_update":
+                global auto_update
+                auto_update = value.lower().startswith("t")
             else:
-                print("Ignoring key '" + key + "'")
-        
-        if os.path.isfile(path + "/tf/cfg/autoexec.cfg"):
-            with open(path + "/tf/cfg/autoexec.cfg", "r", encoding="utf-8") as f:
-                lines = f.readlines()
-            with open(path + "/tf/cfg/autoexec.cfg", "w", encoding="utf-8") as f:
-                found_status_keybind = False
-                found_votekick_keybind = False
-                for line in lines:
-                    write_line = True
-                    if line.startswith("bind \""):
-                        if line.endswith("\" \"status\"\n"):
-                            if line == "bind \"" + tf2_status_keybind + "\" \"status\"\n":
-                                found_status_keybind = True
-                            else:
-                                write_line = False
-                        elif line.endswith("\" \"exec votekick.cfg\""):
-                            if line == "bind \"" + tf2_votekick_keybind + "\" \"exec votekick.cfg\"":
-                                found_votekick_keybind = True
-                            else:
-                                write_line = False
-                    if write_line:
-                        f.write(line)
-                if not found_status_keybind:
-                    f.write("\nbind \"" + tf2_status_keybind + "\" \"status\"")
-                if not found_votekick_keybind:
-                    f.write("\nbind \"" + tf2_votekick_keybind + "\" \"exec votekick.cfg\"")
-        else:
-            with open(path + "/tf/cfg/autoexec.cfg", "w", encoding="utf-8") as f:
-                f.write("bind \"" + tf2_status_keybind + "\" \"status\"\n")
-                f.write("bind \"" + tf2_votekick_keybind + "\" \"exec votekick.cfg\"")
-    with open(path + "/tf/console.log", "w", encoding="utf-8", errors="ignore") as f:
-        f.close()
+                print("Ignoring key '" + key + "'")                    
+
+
+def check_update():
+    global auto_update
+    if auto_update:
+        files = list()
+        files.append(requests.get("https://raw.githubusercontent.com/boyonkgit/tf2-bot-kicker/main/metadata.properties").text.split("\n"))
+        with open("metadata.properties", "r",encoding="utf-8") as f:
+            files.append(f.readlines())
+        for i in range(len(files)):
+            mapped = dict()
+            file = files[i]
+            for line in file:
+                if not line.startswith("#") and not line.strip() == "":
+                    splitter = line.find("=")
+                    key = line[:splitter]
+                    value = line[splitter+1:].replace("\n","")
+                    if key == "version":
+                        mapped[key] = value
+            if i == 0:
+                online_metadata = mapped
+            else:
+                metadata = mapped
+        if not metadata["version"] == online_metadata["version"]:
+            update()
+
+
+def update():
+    new_metadata =  requests.get("https://raw.githubusercontent.com/boyonkgit/tf2-bot-kicker/main/metadata.properties").text.replace("\r","")
+    new_code = requests.get("https://raw.githubusercontent.com/boyonkgit/tf2-bot-kicker/main/code.py").text.replace("\r","")
+    
+    with open("metadata.properties", "w", encoding="utf-8") as f:
+        f.write(new_metadata)
+
+    with open("code.py", "w", encoding="utf-8") as f:
+        f.write(new_code)
+    
+    exec(open("code.py",encoding="utf-8").read())
+    exit()
+def setup_autoexec():
+    if not os.path.isfile(path + "/tf/cfg/autoexec.cfg"):
+        with open(path + "/tf/cfg/autoexec.cfg", "w", encoding="utf-8") as f:
+            f.close()
+
+    with open(path + "/tf/cfg/autoexec.cfg", "r", encoding="utf-8") as f:
+        lines = f.readlines()
+    bk_autoexec_found = False
+    for line in lines:
+        if line.startswith("exec \"bk_autoexec.cfg\""):
+            bk_autoexec_found = True
+            break
+    if not bk_autoexec_found:
+        with open(path + "/tf/cfg/autoexec.cfg", "a", encoding="utf-8") as f:
+            f.write("\nexec \"bk_autoexec.cfg\"")
+
+  
+def create_bk_autoexec():
+    with open(path + "/tf/cfg/bk_autoexec.cfg", "w", encoding="utf-8") as f:
+        f.write("bind \"" + tf2_query_keybind + "\" \"exec bk_query.cfg\"\n")
+        f.write("bind \"" + tf2_execute_keybind + "\" \"exec bk_execute.cfg\"\n")
+        f.write("con_logfile console.log")
+
+
+def create_bk_query():
+    with open(path + "/tf/cfg/bk_query.cfg", "w", encoding="utf-8") as f:
+        f.write("status")
 
 def get_bots():
     global botnames
+    global botregexnames
     global botsteamids
     global online_database
+    
+    files = list()
     if online_database:
-        online_file = requests.get("https://raw.githubusercontent.com/boyonkgit/tf2-bot-kicker/main/bots.properties").text.split("\n")
-        for line in online_file:
-            if not line.startswith("#") and not line == "":
+        files.append(requests.get("https://raw.githubusercontent.com/boyonkgit/tf2-bot-kicker/main/bots.properties").text.split("\n"))
+    with open("bots.properties", "r",encoding="utf-8") as f:
+        files.append(f.readlines())
+    for file in files:
+        for line in file:
+            if not line.startswith("#") and not line.strip() == "":
                 splitter = line.find("=")
                 key = line[:splitter]
                 value = line[splitter+1:].replace("\n","")
@@ -206,115 +247,121 @@ def get_bots():
                     botnames.append(value)
                 elif key == "steamid":
                     botsteamids.append(value)
-                elif not key == "xname" and not key == "xsteamid":
-                    print("Ignoring key '" + key + "' in online database (do you have an outdated version?)")
-
-    with open("bots.properties", "r",encoding="utf-8") as f:
-        bots_file = f.readlines()
-    for line in bots_file:
-        if not line.startswith("#") and not line == "":
-            splitter = line.find("=")
-            key = line[:splitter]
-            value = line[splitter+1:].replace("\n","")
-            if key == "name":
-                botnames.append(value)
-            elif key == "steamid":
-                botsteamids.append(value)
-            elif key == "xname":
-                try:
-                    botnames.remove(value)
-                except:
-                    print("Name '" + value + "' was not found in list")
-            elif key == "xsteamid":
-                try:
-                    botsteamids.remove(value)
-                except:
-                    print("SteamID '" + value + "' was not found in list")
-            else:
-                print("Ignoring key '" + key + "' in bots.properties")
+                elif key == "regexname":
+                    botregexnames.append(re.compile(value))
+                elif key == "xname":
+                    try:
+                        botnames.remove(value)
+                    except:
+                        print("Name '" + value + "' was not found in list")
+                elif key == "xsteamid":
+                    try:
+                        botsteamids.remove(value)
+                    except:
+                        print("SteamID '" + value + "' was not found in list")
+                else:
+                    print("Ignoring key '" + key + "' in bots.properties")
 
 
+### ---------------- ###
 
-## Setup
-setup()
-get_bots()
-
-## Main Loop
-while True:
-    sleep(sleep_time)
-    
-    ## Quering Status
-    
-    # We send a key press to the system, which TF2 will read.
-    keyboard.send(status_keybind)
+### tick ----------- ###
+def tick():
+    query()
     sleep(1)
-    # Because we bound this key to the command "status", TF2 will output the status in the console.
-    # We then read the console from the console.log file (which we enabled TF2 to output to in our startup settings)
+    get_console()
+    read_console()
+    detect()
+    execute()
+
+
+def query():
+    keyboard.send(query_keybind)
+
+
+def get_console():
+    global raw_console
     with open(path + "/tf/console.log", "r", encoding="utf-8", errors="ignore") as f:
-        console = f.readlines()
-    #Next we clear the console.log file, so we can easily start reading for the next output
+        raw_console = f.readlines()
     with open(path + "/tf/console.log", "w", encoding="utf-8", errors="ignore") as f:
         f.close()
-    
-    ## Reading Console
+
+
+def read_console():
+    global raw_console
     userids = list()
     new_players = list()
-    
-    # We go through every line in the console
-    for line in console:
-        # We know that players in the status command output are always prefixed with a '#'
-        # By default TF2 will also output the column names in a line (which starts with "# userid"), if this is the case, we ignore it
+    for line in raw_console:
         if line.startswith("#") and not line.startswith("# userid"):
             arguments = list()
-            # We split the line with ' ' (space)
             for part in line.rstrip().split(" "):
-                # If the splitted part has characters (aka is not an empty string), we append it to the arguments
                 if len(part) > 0:
                     arguments.append(part)
-            # We make sure that we do not take official Valve bots in consideration (in casual these never appear, but in community servers they can)
             if not arguments[len(arguments)-2] == "BOT":
-                # We know that our first argument is the userid of the player
                 userid = arguments[1]
                 if not userid in userids:
                     userids.append(userid)
-                    # We also know that the playername starts after the first '\"' in the string and that it ends at the last '\"' in the string, so we take everything in between
                     original_name = line[line.find("\"")+1:line.rfind("\"")]
                     name = ""
-                    # We then go through every character in the original name, if it is not in our allowed chars set, we skip it
                     for c in original_name:
                         if c.lower() in chars:
                             name = name + c.lower()
-                    # We then read the formatted time from the string and convert it into a number in seconds
-                    time = timetoint(arguments[len(arguments)-4])
-                    
+                    time = time_to_int(arguments[len(arguments)-4])
                     steamid = arguments[len(arguments)-5]
-                    # Lastly we create and append the new Player Instance
                     new_players.append(PlayerInstance(userid,name,time,steamid))
-                    
-    # If we found any new players in our console, we will update the player list to all the players we found
-    if len(new_players) > 0:
-        players = new_players
-    
-    ## Bot Detecting
-    
+        if len(new_players) > 0:
+            global players
+            players = new_players
+
+def detect():
+    global players
+    global clear_log
     checked_names = list()
-    
-    # We empty the log if enabled
     if clear_log:
         os.system("cls")
-    
-    # Next we go through every PlayerInstance in the list
     for player in players:
-        # If we enabled the printing of players, we do so
         if output_player:
             print(str(player))
-        # If the name of the player is in our list of botnames, we kick it
-        # Else if the steamid of the player is in our list of botsteamids, we kick it
-        # Else if the name of the player matches another player name we already went over, we vote kick the newest player
-        # Else (if nothing is wrong) we append the name to the list of checked players
-        if (player.name in botnames) or (player.steamid in botsteamids):
+        if player.name in botnames:
+            votekick(player)
+        elif player.steamid in botsteamids:
             votekick(player)
         elif player.name in checked_names:
-            votekick(getnewest(player.name))
+            votekick(get_newest(player.name))
         else:
-            checked_names.append(player.name)
+            for regex in botregexnames:
+                if regex.match(player.name):
+                    votekick(player)
+        checked_names.append(player.name)
+
+def votekick(player):
+    global commands
+    if output_votekick:
+        print("Votekicking " + str(player))
+    commands.append("callvote kick " + str(player.userid))
+
+def execute():
+    if len(commands) > 0:
+        create_bk_execute()
+        sleep(1)
+        keyboard.send(execute_keybind)
+    
+
+def create_bk_execute():
+    global commands
+    with open(path + "/tf/cfg/bk_execute.cfg", "w", encoding="utf-8") as f:
+        for command in commands:
+            f.write(command + "\n")
+    commands = list()
+
+### ---------------- ###
+
+### run ----------- ###
+def run():
+    setup()
+    while True:
+        sleep(sleep_time)
+        tick()
+
+
+run()
